8447ed4d800b84f03fcebb87b3597369918f5be3
Jeroen Kransen <jeroen@kransen.nl>
2025-12-05 17:18:52 +0100

    Day 4 finished

--- src/main/scala/Day4.scala
+++ src/main/scala/Day4.scala
@@ -1,5 +1,6 @@
 package nl.kransen.aoc2025
 
+import scala.annotation.tailrec
 import scala.io.Source
 
 object Day4 extends App {
@@ -16,32 +17,65 @@ object Day4 extends App {
                    |.@@@@@@@@.
                    |@.@.@@@.@.""".stripMargin
 
-  val lines: List[String] = testInput.linesIterator.filterNot(_.isBlank).toList
+//  val lines: List[String] = testInput.linesIterator.filterNot(_.isBlank).toList
 
-//  val lines: List[String] = Source.fromResource("day4.txt").getLines().toList
+  val lines: List[String] = Source.fromResource("day4.txt").getLines().toList
+  
+  private type Grid = Array[Array[Boolean]]
 
-  val grid: Array[Array[Boolean]] = lines.map(_.map(_ == '@').toArray).toArray
+  val grid: Grid = lines.map(_.map(_ == '@').toArray).toArray
   val height: Int = grid.length
   val width: Int = grid.head.length
   println(s"height: $height, width: $width")
 
-//  roles.foreach(println)
+  def accessibleGrid(grid: Grid): Grid = {
+    (0 until height).map { row =>
+      (0 until width).map(column =>
+        grid(row)(column) && countNeighbours(grid, row, column) < 4
+      ).toArray
+    }.toArray
+  }
 
-  val accessible = (0 until height).flatMap { row => 
-    (0 until width).map(column => {
-      grid(row)(column) && countNeighbours(row, column) < 4
-    })
-  }.count(identity)
-
-  def countNeighbours(row: Int, col: Int): Int = {
-    val count = (Math.max(0, row - 1) to Math.min(height - 1, row + 1)).flatMap { y => 
+  def countNeighbours(grid: Grid, row: Int, col: Int): Int = {
+    (Math.max(0, row - 1) to Math.min(height - 1, row + 1)).flatMap { y =>
       (Math.max(0, col - 1) to Math.min(width - 1, col + 1))
         .filterNot(x => x == col && y == row)
         .map(x => grid(y)(x))
     }.count(identity)
-    println(s"count: $count")
-    count
+  }
+
+  def updateGrid(grid: Grid, accessible: Grid): Grid = {
+    (0 until height).map { row =>
+      (0 until width).map(column => {
+        grid(row)(column) && !accessible(row)(column)
+      }).toArray
+    }.toArray
+  }
+
+  def printGrid(grid: Grid): Unit = {
+    println
+    (0 until height).foreach { row =>
+      (0 until width).foreach(column => {
+        print(if (grid(row)(column)) '@' else '.')
+      })
+      println
+    }
   }
   
-  println(s"accessible: $accessible")
+  @tailrec
+  def removeAllAccessible(grid: Grid, removed: Int = 0): Int = {
+    printGrid(grid)
+    val accessible = accessibleGrid(grid)
+    val accessibleCount: Int = accessible.flatten.count(identity)
+    if (accessibleCount == 0) {
+      removed
+    } else {
+      val updatedGrid = updateGrid(grid, accessible)
+      removeAllAccessible(updatedGrid, removed + accessibleCount)
+    }
+  }
+  
+  val removed = removeAllAccessible(grid)
+  
+  println(s"Removed: $removed")
 }
---
+++ src/main/scala/Day5.scala
@@ -0,0 +1,5 @@
+package nl.kransen.aoc2025
+
+object Day5 extends App {
+  
+}
